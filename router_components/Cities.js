b29686c014f1fdc91869c27f5ab0b57f67907c53
NiklasReents <Niklas.Reents@gmail.com>
2020-05-08 15:07:13 +0200

    add get endpoint to Cities.js component

--- router_components/Cities.js
+++ router_components/Cities.js
@@ -2,4 +2,22 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../database");
 
+const displayCities = (req, res, next) => {
+  pool
+    .query('SELECT * FROM "city";')
+    .then((data) => res.send(data))
+    .catch((e) => console.log(e));
+};
+
+/*const postCities = (req, res) => {
+  const { name } = req.body;
+  pool
+    .query("INSERT INTO city (name) VALUES ($1);", [name])
+    .then((data) => res.json(data.rows))
+    .catch((e) => res.sendStatus(404));
+};*/
+
+router.get("/", displayCities);
+//router.post("/", postCities);
+
 module.exports = router;
