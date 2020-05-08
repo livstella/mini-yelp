d1d1d02568d65534f16b1ddf57032fbc40aec0d8
NiklasReents <Niklas.Reents@gmail.com>
2020-05-08 16:20:17 +0200

    add post endpoints to components

--- router_components/Cities.js
+++ router_components/Cities.js
@@ -9,15 +9,15 @@ const displayCities = (req, res, next) => {
     .catch((e) => console.log(e));
 };
 
-/*const postCities = (req, res) => {
+const postCities = (req, res) => {
   const { name } = req.body;
   pool
     .query("INSERT INTO city (name) VALUES ($1);", [name])
     .then((data) => res.json(data.rows))
     .catch((e) => res.sendStatus(404));
-};*/
+};
 
 router.get("/", displayCities);
-//router.post("/", postCities);
+router.post("/", postCities);
 
 module.exports = router;
