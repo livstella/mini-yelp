1032e5b18134a87634c26ce1b0e5b5d56a15496a
NiklasReents <Niklas.Reents@gmail.com>
2020-05-08 15:42:53 +0200

    add get endpoint to Tags.js component

--- router_components/Tags.js
+++ router_components/Tags.js
@@ -2,4 +2,13 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../database");
 
+const displayTags = (req, res, next) => {
+  pool
+    .query('SELECT * FROM "tag";')
+    .then((data) => res.send(data))
+    .catch((e) => console.log(e));
+};
+
+router.get("/", displayTags);
+
 module.exports = router;
